5fad178011cd643b8183837e6e87d6be2ecf93e9
JugerHiTech <juger.hitech@msa.hinet.net>
2017-11-22 17:14:41 +0800

    Swagger

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 using HelloAngular.Models;
 
 namespace HelloAngular
@@ -27,11 +28,34 @@ namespace HelloAngular
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "My HelloAngular API",
+                    Description = "Hello Angular ASP.NET Core 2 WebAPI",
+                    TermsOfService = "NONE",
+                    Contact = new Contact { Name = "Edwin", Email = "edwin.liu@printech.com", Url = "www.djiprintech.com" },
+                    License = new License { Name = "DJIT", Url = "www.djiprintech.com" }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to server swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My HelloAngular API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
